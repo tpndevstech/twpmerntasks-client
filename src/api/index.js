4786f384813ffdf5821da893b6c02b59c08e12f5
Tapan Anand <70485285+tpndevstech@users.noreply.github.com>
2022-06-25 22:28:05 +0530

    Update index.js

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,9 @@
 import axios from 'axios';
 
 const url = 'https://twpmerntasks-server.herokuapp.com/posts';
+//this url should be changed as per local or hosted status
+//for eg: if local, then url would be something like 'https://localhost:<port>/"
+//but if hosted online , then url would be where backend code is deployed and hosted 
 
 export const fetchPosts = () => axios.get(url);
 export const createPost = (newPost) => axios.post(url, newPost);
